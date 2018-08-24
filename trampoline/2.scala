0c99d7a9b31b7a0759575c02f459fbdd3ef44a55
Tushar Mathur <tusharmath@gmail.com>
2018-08-24 06:26:38 +0530

    feat(trampoline): add v2

---
+++ trampoline/2.scala
@@ -0,0 +1,25 @@
+import scala.language.higherKinds
+
+
+object Main extends App {
+  sealed trait Trampoline[A]
+  case class Done[A](value: A) extends Trampoline[A]
+  case class More[A](call: () => Trampoline[A]) extends Trampoline[A]
+
+  def even[A](l: List[A]): Trampoline[Boolean] = l match {
+    case Nil => Done(true)
+    case x :: xs => More(() => odd(xs))
+  }
+
+  def odd[A](l: List[A]): Trampoline[Boolean] = l match {
+    case Nil => Done(false)
+    case x :: xs => More(() => even(xs))
+  }
+
+  def run[A](trampoline: Trampoline[A]): A = trampoline match {
+    case Done(v) => v
+    case More(t) => run(t()) // <- tail recursive, yay
+  }
+
+  println(run(even((0 to 100000 - 1).toList)))
+}
